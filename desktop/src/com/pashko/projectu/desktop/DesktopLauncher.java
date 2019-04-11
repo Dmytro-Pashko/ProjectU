ae3250a711a6f168c1d70a3df76a747ebed77970
Dmytro Pashko <pashko.dmytro@gmail.com>
2019-04-11 23:42:17 +0300

    Init commit.

---
+++ desktop/src/com/pashko/projectu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pashko.projectu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pashko.projectu.Loader;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Loader(), config);
+	}
+}
